e90715584cc21d5e2ebf69fd40d17127b69761cc
Dmytro Shkatula <idnol84@gmail.com>
2024-02-11 23:16:54 +0200

    update

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const {getCurrentUser, logout} = require("../controllers/usersController");
+const validateBody = require("../helpers/validateBody");
+const {userSchemas} = require('../models/user');
+const {register, login, getCurrentUser, logout} = require('../controllers/usersController');
 const authenticate = require("../helpers/authenticate");
 
+const {registerSchema, loginSchema} = userSchemas;
 
 const usersRouter = express.Router();
 
+usersRouter.post('/register', validateBody(registerSchema), register);
+usersRouter.post('/login', validateBody(loginSchema), login);
 usersRouter.get('/current', authenticate, getCurrentUser);
 usersRouter.get('/logout', authenticate, logout);
 
